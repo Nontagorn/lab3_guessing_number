2cf379e526501ed9b300c393d4d6900ea9ca0103
Pongpisut1999 <nattha_c@elearning.cmu.ac.th>
2020-01-18 23:40:20 +0700

    4th

--- index.js
+++ index.js
@@ -9,7 +9,9 @@
         var x = document.getElementById("guessField").value; 
  
         if(guess<=10){
-                  
+            if(x > 100 || x < 1){
+                document.querySelector('.result').textContent = "Please Enter Number Between 0 and 100"; 
+            }else{
                 document.querySelector('.turn').textContent = 10 - guess + " left";
                 if(x == y){     
                     document.querySelector('.result').textContent = "CONGRATULATIONS!!! YOU GUESSED IT RIGHT IN " + guess + " GUESS ";                   
@@ -27,7 +29,7 @@
                         guessed += ", "+x;
                     }
                     document.querySelector('.history').textContent = guessed; 
-                        
+            }                
         }
 
         if(guess == 11){
@@ -35,3 +37,5 @@
         }
     
     }
+
+    
